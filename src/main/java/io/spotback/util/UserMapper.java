20103fa1c89df9f1bf8b3cef547ca0da43b50412
Spotback <spotbackteam@gmail.com>
2019-03-24 00:29:56 -0700

    email update support

--- src/main/java/io/spotback/util/UserMapper.java
+++ src/main/java/io/spotback/util/UserMapper.java
@@ -2,11 +2,19 @@ package io.spotback.util;
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTableMapper;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import io.spotback.pojos.User;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 
 public class UserMapper extends Mapper {
@@ -39,6 +47,13 @@ public class UserMapper extends Mapper {
     public void delete(JsonObject dltUser) {
         User user = dltUser.mapTo(User.class);
         mapper.deleteIfExists(user);
+    }
+
+    public void updateEmail(String oldEmail, String newEmail) {
+        User user = read(new JsonObject().put("email", oldEmail));
+        delete(JsonObject.mapFrom(user));
+        user.setEmail(newEmail);
+        create(JsonObject.mapFrom(user));
     }
 }
 
